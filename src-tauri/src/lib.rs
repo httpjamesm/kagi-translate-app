055a070eb8046fccc7cd813f37a69029ff2fde04
httpjamesm <github@httpjames.space>
2024-11-17 19:06:26 -0500

    fix: conditional haptics

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -118,8 +118,7 @@ fn get_migrations() -> Vec<Migration> {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_haptics::init())
+    let mut builder= tauri::Builder::default()
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(
             tauri_plugin_sql::Builder::default()
@@ -137,11 +136,19 @@ pub fn run() {
         })
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![
-            detect_language,
-            get_translation,
-            set_session_token,
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+
+        #[cfg(any(target_os = "android", target_os = "ios"))]
+        {
+            builder = builder
+                .plugin(tauri_plugin_haptics::init());
+        }
+
+        builder
+            .invoke_handler(tauri::generate_handler![
+                detect_language,
+                get_translation,
+                set_session_token,
+            ])
+            .run(tauri::generate_context!())
+            .expect("error while running tauri application");
 }
